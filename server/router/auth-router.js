e8b934611b4f710a10a0b071182bf09e2f8fe861
Ankit Kumar <abhishekankit128@gmail.com>
2024-03-08 22:56:24 +0530

    added ZOD for validation {email, userName, password, phone number, etc

--- server/router/auth-router.js
+++ server/router/auth-router.js
@@ -1,14 +1,15 @@
 const express = require("express");
 const router = express.Router();
-const { home, Registration, Login } = require("../controllers/auth-controller");
-
+const authCotrollers = require("../controllers/auth-controller");
+const signupSchema = require("../validators/auth-validator");
+const validate = require("../middleware/validate-middleware");
 //for Home Page
-router.route("/").get(home);
+router.route("/").get(authCotrollers.home);
 
 //Rout API for register page
-router.route('/register').post(Registration); //for Registration Page
+router.route('/register').post(validate(signupSchema), authCotrollers.Registration);
 
 //Rout API for login page
-router.post('/login', Login);
+router.route("/login").post(authCotrollers.Login);
 
 module.exports = router;
---
+++ server/validators/auth-validator.js
@@ -0,0 +1,26 @@
+const { z } = require("zod");
+
+//creating an object schema
+const signupSchema = z.object({
+    username: z
+        .string({ required_error: "Username is required" })
+        .trim()
+        .min(3, "Username must be at least 3 characters")
+        .max(20, "Username cannot exceed 20 characters"),
+    email: z
+        .string({ required_error: "Email is required" })
+        .trim()
+        .email("Please enter a valid email address"),
+    password: z
+        .string({ required_error: "Password is required" })
+        .trim()
+        .min(8, "Password should have at least 8 characters")
+        .max(15, "Password cannot exceed 15 characters"),
+    phoneNumber: z
+        .string({ required_error: "Phone Number is required" })
+        .trim()
+        .min(10, "Phone Number should have at least 8 characters")
+        .max(20, "Phone Number cannot exceed 15 characters"),
+});
+
+module.exports = signupSchema;
