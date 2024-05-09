fc9590186d9f105d76f1207f1dcb0cfc871d1de7
ANKIT KUMAR <144757750+128-Ankit@users.noreply.github.com>
2024-05-09 15:50:53 +0530

    Update error-middleware.js

--- server/middleware/error-middleware.js
+++ server/middleware/error-middleware.js
@@ -1,13 +1,16 @@
 const erroMiddleware = (err, req, res, next) => {
-    const status = err.status || 500;
-    const message = err.message || "BACKEND ERROR";
-    const extraDetails = err.extraDetails || "Error form backend";
-
-    return res.status(status).json({
-        message,
-        extraDetails
-    });
-
+    try{
+         const status = err.status || 500;
+        const message = err.message || "BACKEND ERROR";
+        const extraDetails = err.extraDetails || "Error form backend";
+    
+        return res.status(status).json({
+            message,
+            extraDetails
+        });
+    }
+    catch (err){
+        console.log(err)
 };
 
 module.exports = erroMiddleware;
